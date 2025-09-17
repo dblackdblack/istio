8a8972ccb3a3c74e0e166157f5df4bb0ddd6cdf3
David Black <dblack@benchling.com>
2025-09-17 09:58:56 -0700

    add checks

--- tools/istio-iptables/pkg/dependencies/implementation_linux.go
+++ tools/istio-iptables/pkg/dependencies/implementation_linux.go
@@ -32,7 +32,7 @@ import (
 	"istio.io/istio/tools/istio-iptables/pkg/constants"
 )
 
-var testRuleAdd = []string{"-t", "nat", "-A", "INPUT", "-p", "255", "-j", "RETURN", "-m", "comment", "--comment", `"Istio no-op iptables capability probe"`}
+var testRuleAdd = []string{"-t", "nat", "-A", "INPUT", "-p", "255", "-j", "RETURN"}
 
 // TODO the entire `istio-iptables` package is linux-specific, I'm not sure we really need
 // platform-differentiators for the `dependencies` package itself.
@@ -232,6 +232,17 @@ func mount(src, dst string) error {
 func (r *RealDependencies) executeXTables(log *log.Scope, cmd constants.IptablesCmd, iptVer *IptablesVersion,
 	silenceErrors bool, stdin io.ReadSeeker, args ...string,
 ) (*bytes.Buffer, error) {
+	// Prevent iptables comment flags from being used because gVisor does not support them.
+	// See: https://github.com/istio/istio/issues/57678
+	for i, arg := range args {
+		if arg == "-m" && i+1 < len(args) && args[i+1] == "comment" {
+			return nil, fmt.Errorf("iptables comment flags are not allowed: found '-m comment' in arguments")
+		}
+		if arg == "--comment" {
+			return nil, fmt.Errorf("iptables comment flags are not allowed: found '--comment' in arguments")
+		}
+	}
+
 	mode := "without lock"
 	stdout := &bytes.Buffer{}
 	stderr := &bytes.Buffer{}
--- tools/istio-iptables/pkg/dependencies/implementation_linux_test.go
+++ tools/istio-iptables/pkg/dependencies/implementation_linux_test.go
@@ -15,6 +15,7 @@
 package dependencies
 
 import (
+	"strings"
 	"testing"
 
 	// Create a new network namespace. This will have the 'lo' interface ready but nothing else.
@@ -22,9 +23,12 @@ import (
 	// Create a new user namespace. This will map the current UID to 0.
 	_ "github.com/howardjohn/unshare-go/userns"
 	"github.com/vishvananda/netns"
+	utilversion "k8s.io/apimachinery/pkg/util/version"
 
+	"istio.io/istio/pkg/log"
 	"istio.io/istio/pkg/test/util/assert"
 	"istio.io/istio/pkg/test/util/file"
+	"istio.io/istio/tools/istio-iptables/pkg/constants"
 )
 
 func TestRunInSandbox(t *testing.T) {
@@ -47,4 +51,85 @@ func TestRunInSandbox(t *testing.T) {
 	assert.Equal(t, sandboxed, "")
 	assert.Equal(t, original, after)
 	assert.Equal(t, originalNetNS.Equal(sandboxedNetNS), true)
+}
+
+func TestExecuteXTablesCommentPrevention(t *testing.T) {
+	deps := &RealDependencies{}
+	logger := log.FindScope(log.DefaultScopeName)
+
+	// Create a mock iptables version for testing
+	mockVersion := &IptablesVersion{
+		DetectedBinary:        "iptables",
+		DetectedSaveBinary:    "iptables-save",
+		DetectedRestoreBinary: "iptables-restore",
+		Version:               utilversion.MustParseGeneric("1.8.4"),
+		Legacy:                true,
+		ExistingRules:         false,
+	}
+
+	testCases := []struct {
+		name        string
+		args        []string
+		expectError bool
+		errorText   string
+	}{
+		{
+			name:        "comment module with -m comment flag should be rejected",
+			args:        []string{"-t", "nat", "-A", "INPUT", "-m", "comment", "--comment", "test comment"},
+			expectError: true,
+			errorText:   "iptables comment flags are not allowed: found '-m comment' in arguments",
+		},
+		{
+			name:        "standalone --comment flag should be rejected",
+			args:        []string{"-t", "nat", "-A", "INPUT", "--comment", "test comment"},
+			expectError: true,
+			errorText:   "iptables comment flags are not allowed: found '--comment' in arguments",
+		},
+		{
+			name:        "comment flag at the end should be rejected",
+			args:        []string{"-t", "nat", "-A", "INPUT", "-j", "ACCEPT", "-m", "comment"},
+			expectError: true,
+			errorText:   "iptables comment flags are not allowed: found '-m comment' in arguments",
+		},
+		{
+			name:        "valid rule without comment should be accepted",
+			args:        []string{"-t", "nat", "-A", "INPUT", "-p", "tcp", "-j", "ACCEPT"},
+			expectError: false,
+		},
+		{
+			name:        "rule with other -m modules should be accepted",
+			args:        []string{"-t", "nat", "-A", "INPUT", "-m", "state", "--state", "ESTABLISHED"},
+			expectError: false,
+		},
+		{
+			name:        "rule with multiple -m modules but no comment should be accepted",
+			args:        []string{"-t", "nat", "-A", "INPUT", "-m", "tcp", "-m", "state", "--state", "NEW"},
+			expectError: false,
+		},
+		{
+			name:        "word comment in other contexts should be accepted",
+			args:        []string{"-t", "nat", "-A", "INPUT", "-p", "tcp", "--dport", "80", "-j", "LOG", "--log-prefix", "comment-test"},
+			expectError: false,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			_, err := deps.executeXTables(logger, constants.IPTables, mockVersion, true, nil, tc.args...)
+
+			if tc.expectError {
+				assert.Error(t, err)
+				if err != nil {
+					assert.Equal(t, strings.Contains(err.Error(), tc.errorText), true, "Expected error message to contain: %s, got: %v", tc.errorText, err.Error())
+				}
+			} else {
+				// Note: These tests may fail with actual iptables execution errors since we're not mocking the exec.Command
+				// But they should NOT fail with our comment prevention error
+				if err != nil {
+					assert.Equal(t, strings.Contains(err.Error(), "iptables comment flags are not allowed"), false,
+						"Should not fail due to comment prevention, but got: %v", err.Error())
+				}
+			}
+		})
+	}
 }
